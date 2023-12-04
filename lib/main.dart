ba6b7a3dcad40434ac03907ae003591853d53204
EmRenRen <dylan.sengchanh@gmail.com>
2023-12-04 17:55:36 +0100

    fix snackbar pour qu'il reste longtemps +reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'boss_widget.dart';
+
 void main() {
   runApp(FrenchQuizApp());
 }
@@ -39,7 +40,8 @@ class _QuizPageState extends State<QuizPage> {
   double _bossHealth = 100; // Boss health
   double _maxBossHealth = 100.0; // Define the maxBossHealth variable
   int _consecutiveGoodAnswers = 0; // Add the consecutiveGoodAnswers variable
-  double _healthImpactPerQuestion = 10.0; // Define the health impact per question
+  double _healthImpactPerQuestion =
+      10.0; // Define the health impact per question
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +71,6 @@ class _QuizPageState extends State<QuizPage> {
                         'Score: $_score',
                         style: TextStyle(fontSize: 24.0),
                       ),
-                      
                     ],
                   )
                 : Column(
@@ -97,7 +98,8 @@ class _QuizPageState extends State<QuizPage> {
                   ),
             SizedBox(height: 20.0),
             LinearProgressIndicator(
-              value: _bossHealth / 100, // Replace 100 with the maximum boss health value
+              value: _bossHealth /
+                  100, // Replace 100 with the maximum boss health value
               backgroundColor: Colors.grey,
               valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
             ),
@@ -119,25 +121,26 @@ class _QuizPageState extends State<QuizPage> {
           selectedAnswer == _questions[_currentQuestionIndex].correctAnswer;
       _showResult = true;
 
-       if (_questions[_currentQuestionIndex].isCorrect) {
+      if (_questions[_currentQuestionIndex].isCorrect) {
         _score++; // Increment the score by 1 for each correct answer
-        _score += (_score * _scoreMultiplier).toInt(); // Increment the score by the value of the score multiplier
+        _score += (_score * _scoreMultiplier)
+            .toInt(); // Increment the score by the value of the score multiplier
         _scoreMultiplier += 0.5; // Increment the score multiplier by 0.5
         _consecutiveGoodAnswers++;
 
-         // Check if the consecutive good answers count is greater than a threshold
-      if (_consecutiveGoodAnswers >= 3) {
-         _scoreMultiplier = (_scoreMultiplier + 0.5).clamp(1.0, 2.0);
-        // Decrease the boss health based on the consecutive good answers
-        _bossHealth -= _healthImpactPerQuestion * _scoreMultiplier;
-        
-      } else {
-        // Decrease the boss health by the regular health impact per question
-        _bossHealth -= _healthImpactPerQuestion;
-      }
+        // Check if the consecutive good answers count is greater than a threshold
+        if (_consecutiveGoodAnswers >= 3) {
+          _scoreMultiplier = (_scoreMultiplier + 0.5).clamp(1.0, 2.0);
+          // Decrease the boss health based on the consecutive good answers
+          _bossHealth -= _healthImpactPerQuestion * _scoreMultiplier;
+        } else {
+          // Decrease the boss health by the regular health impact per question
+          _bossHealth -= _healthImpactPerQuestion;
+        }
       } else {
         _scoreMultiplier = 0.0; // Reset the score multiplier
-        _consecutiveGoodAnswers = 0;  // Reset the consecutive good answers count if the answer is incorrect
+        _consecutiveGoodAnswers =
+            0; // Reset the consecutive good answers count if the answer is incorrect
       }
     });
   }
@@ -145,44 +148,65 @@ class _QuizPageState extends State<QuizPage> {
   void _nextQuestion() {
     setState(() {
       if (_bossHealth <= 0) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Container(
-            child: Row(
-              children: [
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-                SizedBox(width: 8.0),
-                Text(
-                  'You win!',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Container(
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.yellow,
                   ),
-                ),
-              ],
+                  SizedBox(width: 8.0),
+                  Text(
+                    'You win!',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            duration: Duration(days: 365), // Set a very long duration to make the SnackBar stay visible
+            backgroundColor: Colors.green,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            behavior: SnackBarBehavior.floating,
+            margin: EdgeInsets.all(16.0),
+            padding: EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 16.0,
+            ),
+            action: SnackBarAction(
+              label: 'Restart',
+              onPressed: () {
+                // Reset the game
+                _resetGame();
+              },
             ),
           ),
-          duration: Duration(seconds: 2),
-          backgroundColor: Colors.green,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          behavior: SnackBarBehavior.floating,
-          margin: EdgeInsets.all(16.0),
-          padding: EdgeInsets.symmetric(
-            vertical: 12.0,
-            horizontal: 16.0,
-          ),
-        ),
-      );
-      // Reset the game or navigate to a new screen
-      return;
-    }
-      _currentQuestionIndex = Random().nextInt(_questions.length); // Randomly select next question
+        );
+        // Reset the game or navigate to a new screen
+        return;
+      }
+      _currentQuestionIndex =
+          Random().nextInt(_questions.length); // Randomly select next question
       _showResult = false;
+    });
+  }
+
+  void _resetGame() {
+    // Add logic to reset the game state
+    // For example, reset the boss health, current question index, and other relevant variables
+    setState(() {
+      _bossHealth = 100;
+      _currentQuestionIndex = 0;
+      _showResult = false;
+      _score = 0;
+      _scoreMultiplier = 0.0;
+      _consecutiveGoodAnswers = 0;
     });
   }
 }
